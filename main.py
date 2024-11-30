6a5d7b65b4aeca470636e8f88d020207f46a1809
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2024-11-30 14:25:54 +0100

    remove repos nilearn's repos

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ from searcher import read_token_from_file, search_repositories
 from cloner import clone_repository
 from analyser import count_imports
 import os
+from tqdm import tqdm
 
 print("Searching for repositories...\n")
 token_file_path = "token"
 token = read_token_from_file(token_file_path)
 query = "nilearn"
 repos = search_repositories(query, token)
-print(f"Found {repos['total_count']} repositories for query '{query}'.\n")
+# find repos whose owner is the query
+repos_owned_by_query = []
+for repo in repos:
+    if repo["owner"]["login"] == query:
+        repos_owned_by_query.append(repo)
+print(f"Found {len(repos_owned_by_query)} repositories owned by '{query}'.\n")
+print(
+    f"Removing repositories owned by the {query} from the list of "
+    "repositories...\n"
+)
+# remove repos owned by query from the list of repos
+for repo in repos_owned_by_query:
+    repos.remove(repo)
+print(f"Remaining repositories: {len(repos)}\n")
 
 print("Cloning repositories...\n")
 os.makedirs("repos", exist_ok=True)
-for repo in repos["items"]:
+for repo in tqdm(repos):
     clone_repository(repo["clone_url"], f"./repos/{repo['name']}")
 
 print("Analyzing imports...\n")
